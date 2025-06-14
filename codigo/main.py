7c6e4ab1b142eaad2689be841a50363633eb336a
alsa-ayrt <ayrton.luis.sa@gmail.com>
2025-06-14 15:46:10 -0300

    Added game_map and tile_types

--- codigo/main.py
+++ codigo/main.py
@@ -1,44 +1,37 @@
 import tcod
-from actions import EscapedAction, MovementAction
+
+from engine import Engine
 from input_handlers import EventHandler
-from entity import Entity
+from codigo.actors.entity import Entity
+from codigo.tiles.game_map import GameMap
 
 
 def main():
 
-    width = 80
-    height = 80
-
+    width = 60
+    height = 60
+    map_width = 60
+    map_height = 55
 
     tileset = tcod.tileset.load_tilesheet("img1.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
 
     event_handler = EventHandler()
 
-    player = Entity(int(width / 2 ), int(height / 2), "@", (255, 255, 255))
+    player = Entity(int(width / 2), int(height / 2), "@", (255, 255, 255))
     npc = Entity(1, 1, "@", (255, 255, 0))
     entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new(columns=width, rows=height, tileset=tileset, title="Ayrtonikus", vsync=True,) as context:
         root_console = tcod.console.Console(width, height, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
-            root_console.clear()
-
-            for event in tcod.event.wait():
-
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-
-                    player.move(dx=action.dx, dy=action.dy)
-
-                elif isinstance(action, EscapedAction):
-                    raise SystemExit
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
---
+++ codigo/tiles/game_map.py
@@ -0,0 +1,20 @@
+import numpy as np
+from codigo.tiles import tile_types
+from tcod.console import Console
+
+
+class GameMap:
+    def __init__(self, width: int, heigth: int):
+        self.width = width
+        self.height = heigth
+        self.tiles = np.full((width, heigth), fill_value=tile_types.floor, order="F")
+
+        self.tiles[30:33, 22] = tile_types.wall
+
+    def in_bounds(self, x: int, y: int):
+
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def render(self, console: Console) -> None:
+        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+
